71f31402040952ec809c9673d0105532e140d847
양세윤 <dosy13@naver.com>
2024-07-30 10:48:40 +0900

    글목록 출력하기

--- main.js
+++ main.js
@@ -2,18 +2,31 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-const { log } = require('console');
-var app = http.createServer(function(request,response){
+const {log} = require('console');
+var app = http.createServer(function (request, response) {
     var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathName = url.parse(_url, true).pathname;
+    var queryData = url
+        .parse(_url, true)
+        .query;
+    var pathName = url
+        .parse(_url, true)
+        .pathname;
 
-    if(pathName === '/') {
-      if(!queryData.id) {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = 'Welcome!';
-          var description = 'Hello, node.js!'
-          var template = `
+    if (pathName === '/') {
+        if (!queryData.id) {
+
+            fs.readdir('./data', (error, fileList) => {
+                var title = 'Welcome!';
+                var description = 'Hello, node.js!'
+                var list = '<ul>';
+                var i = 0;
+                while (i < fileList.length) {
+                    list = list + `<li><a href="?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+
+                var template = `
         <!doctype html>
         <html>
         <head>
@@ -22,53 +35,52 @@ var app = http.createServer(function(request,response){
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
+          ${list}
           <h2>${title}</h2>
           <p>${description}</p>
         </body>
         </html>
         `;
-        response.writeHead(200);
-        response.end(template);
-        });
-      } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-        });
-      }
-
-
-      
+                response.writeHead(200);
+                response.end(template);
+            })
+        } else {
+            fs.readdir('./data', (error, fileList) => {
+                var title = 'Welcome!';
+                var description = 'Hello, node.js!'
+                var list = '<ul>';
+                var i = 0;
+                while (i < fileList.length) {
+                    list = list + `<li><a href="?id=${fileList[i]}">${fileList[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+                fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                    var title = queryData.id;
+                    var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+                    response.writeHead(200);
+                    response.end(template);
+                });
+            });
+        }
     } else {
-      response.writeHead(404);
-      response.end('Not found');
+        response.writeHead(404);
+        response.end('Not found');
     }
 
-
 });
-app.listen(80);
+app.listen(5000);
