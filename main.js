fd0c794ca023038c1d845d04ab582e0068c16438
양세윤 <dosy13@naver.com>
2024-08-05 19:04:09 +0900

    post 방식으로 전송된 데이터 받기

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 const templateHtml = (title, list, body) => {
     return `
@@ -81,6 +82,24 @@ var app = http.createServer(function (request, response) {
             response.writeHead(200);
             response.end(template);
         });
+    } else if (pathName === '/create_process') {
+      var body = '';
+      request.on('data', function(data){
+        // 웹브라우저가 포스트 방식으로 데이터를 전송할 때 데이터의 양이 많을 경우 프로그램이 꺼지거나 컴퓨터에 무리가 가는 것을 방지하여 사용하는 것
+        // 웹브라우저에서 데이터를 받을 때 콜백 함수가 실행되고, data 안에 전달 받은 값이 들어있음
+        body = body + data;
+      });
+      request.on('end', function(data){
+        // 더 이상 데이터가 들어오지 않으면 해당 콜백함수가 실행됨
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+        console.log('title:' , title);    
+        console.log('description:' , description);    
+      });
+      response.writeHead(202);
+      response.end('create_process');
+
     } else {
         response.writeHead(404);
         response.end('Not found');
