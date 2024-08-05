310ee460c137282363cfbb28501af9590575e927
양세윤 <dosy13@naver.com>
2024-08-05 21:04:47 +0900

    파일생성 및 리다이렉션

--- main.js
+++ main.js
@@ -94,12 +94,11 @@ var app = http.createServer(function (request, response) {
         var post = qs.parse(body);
         var title = post.title;
         var description = post.description;
-        console.log('title:' , title);    
-        console.log('description:' , description);    
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {location: `/?id=${title}`});
+          response.end();
+        });
       });
-      response.writeHead(202);
-      response.end('create_process');
-
     } else {
         response.writeHead(404);
         response.end('Not found');
